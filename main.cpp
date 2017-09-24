b30efece0612c1162716889f7de1b9b999ef7c67
nomaterials <nomaterials@gmail.com>
2017-09-24 18:27:06 +0300

    add graphviz visualisation (png) for results

--- main.cpp
+++ main.cpp
@@ -8,6 +8,11 @@
 #include <boost/graph/graphviz.hpp>
 #include <boost/graph/reverse_graph.hpp>
 #include <boost/graph/graph_utility.hpp>
+#include <boost/graph/push_relabel_max_flow.hpp>
+#include <LEDA/core/string.h>
+#include <LEDA/system/misc.h>
+//#include <LEDA/graph/max_flow.h>
+
 
 
 
@@ -15,7 +20,7 @@ using namespace boost;
 using namespace std;
 
 
-//Type & Struct Definitions -----------------------------------------------------------------
+//Type & Struct Definitions ___________________
 
 // Bundled properties for graph
 struct bundleVertex {
@@ -28,9 +33,11 @@ struct bundleEdge {
     int residual;
 };
 
+struct bundleGraph;
+
 
 // Define graph type
-typedef adjacency_list<vecS, vecS, bidirectionalS, bundleVertex, bundleEdge> Graph;
+typedef adjacency_list<vecS, vecS, bidirectionalS, bundleVertex, bundleEdge, bundleGraph> Graph;
 
 //Define graph traits type
 typedef graph_traits<Graph> Traits;
@@ -41,20 +48,96 @@ typedef Traits::vertex_descriptor Vertex;
 typedef Traits::edge_descriptor Edge;
 
 
+struct bundleGraph {
+    Vertex source;
+    Vertex sink;
+};
 
 
-// Function Definitions -----
+// Function Declarations ___________________
+
+// Create example graph used for visualising
+Graph createExampleGraph();
+// Print results to terminal
+void printResults(Graph &G);
+// Create graph visualisation dot language file
+void visualise(Graph &G);
+
+
+
+
+//Excess Scale functions -------
+void ExcessScaleMaxFlow(Graph &G, Vertex source, Vertex sink);
+
+// Handle preprocessing operations
 void preprocess(Graph &G, Vertex source, Vertex sink);
-
+// Calculate initial delta = 2 ^ log(maxCapacity)
+float calcInitialDelta(Graph &G);
 // Utility function for selecting node with smallest distance among nodes with large excess
 Vertex selectNode(Graph &G, Vertex source, Vertex sink, float delta);
-
 // implements push of flow or relabel of distance of current node
 void pushRelabel(Graph &G, Vertex current, float delta);
 
 
 
+// Custom Property Writer for Graphviz
+template <class IdMap, class ExcessMap, class DistanceMap>
+class vertex_writer {
+public:
+    vertex_writer(IdMap i, ExcessMap e, DistanceMap d) : im(i), em(e), dm(d) {}
+    template <class Vertex>
+    void operator()(ostream &out, const Vertex& v) const {
+        out << "[label=\"ID:" << im[v] << "\nExcess: "<< em[v] <<"\nDistance: "<< dm[v] << "\"]";
+    }
+private:
+    IdMap im;
+    ExcessMap em;
+    DistanceMap dm;
+};
+// Function for passing in the maps to custom property writer
+template <class IdMap, class ExcessMap, class DistanceMap>
+inline vertex_writer<IdMap, ExcessMap, DistanceMap>
+make_vertex_writer(IdMap i, ExcessMap e, DistanceMap d) {
+    return vertex_writer<IdMap, ExcessMap, DistanceMap>(i, e, d);
+}
+
+
+
+
+
 int main() {
+
+    Graph exampleG = createExampleGraph();
+
+    Vertex source = exampleG[graph_bundle].source;
+    Vertex sink = exampleG[graph_bundle].sink;
+
+
+
+
+
+    /*Algorithm exectution and timing*/
+
+    cout<<"\t\tEXCESS SCALE MAX FLOW ALGORITHM"<<endl;
+    cout << "Case:\tExample Graph"<<endl;
+    float time = leda::used_time();
+    ExcessScaleMaxFlow(exampleG, source, sink);
+    cout << "\nImplementation Time: " << leda::used_time(time) << endl;
+    //MAX_FLOW_T(const graph& G, node s, node t, const edge_array< NT> & cap)
+    //cout << "BGL Time: " << leda::used_time(time) << endl<<endl;
+
+
+
+    visualise(exampleG);  // dot -Tpng ./cmake-build-debug/graph.dot > exampleFinal.png
+
+
+    return 0;
+}
+
+
+
+// Function Definitions ___________________
+Graph createExampleGraph() {
 
     // Graph instantiation
     Graph G;
@@ -71,32 +154,14 @@ int main() {
     add_edge(v2, v4, bundleEdge{1}, G);
     add_edge(v3, v4, bundleEdge{5}, G);
 
+    G[graph_bundle].source = v1;
+    G[graph_bundle].sink = v4;
 
-    // Excess Scaling Algorithm-----
 
-    //Preprocessing ----
-    preprocess(G, v1, v4);  // TODO: REMOVE HARDCODE
+    return G;
+}
 
-    // Find the max capacity to calculate delta = 2 ^ log(maxCapacity)
-    Graph::edge_iterator edgeIt, edgeEnd;
-    tie(edgeIt, edgeEnd) = edges(G);
-    std::vector<int> capacities(num_edges(G));
-    for (; edgeIt!= edgeEnd; ++edgeIt) {
-        capacities.push_back(G[*edgeIt].residual);
-    }
-    int maxCap = *max_element(capacities.begin(), capacities.end());
-    float delta = (float) pow(2, ceil(log2(maxCap)));
-
-    while(delta >= 1) {
-        Vertex currentNode = selectNode(G, v1, v4, delta); // select node with smallest distance among nodes with large excess
-        while(currentNode != 0) {
-            // push flow or relabel distance of current node
-            pushRelabel(G, currentNode, delta);
-            // select another node from updated graph
-            currentNode = selectNode(G, v1, v4, delta);
-        }
-        delta /= 2;  // update delta
-    }
+void printResults(Graph &G) {
 
     print_graph(G, get(&bundleVertex::id, G));
 
@@ -108,14 +173,43 @@ int main() {
     }
 
     //TODO: MAX FLOW IS EXCESS VALUE AT SINK...handle that
+}
 
-
-    return 0;
+void visualise(Graph &G) {
+    ofstream dot("graph.dot");
+    write_graphviz(dot, G, make_vertex_writer(get(&bundleVertex::id, G),
+                                              get(&bundleVertex::excess, G),
+                                              get(&bundleVertex::distance, G)),
+                           make_label_writer(get(&bundleEdge::residual, G)));
 }
 
 
 
-// Function Declarations -----
+
+void ExcessScaleMaxFlow(Graph &G, Vertex source, Vertex sink) {
+
+    // Preprocessing
+    preprocess(G, source, sink);
+
+    // Calculate initial delta = 2 ^ log(maxCapacity)
+    float delta = calcInitialDelta(G);
+
+    // Core update loop
+    while(delta >= 1) {
+
+        // select node with smallest distance among nodes with large excess
+        Vertex currentNode = selectNode(G, source, sink, delta);
+        while(currentNode != 0) {
+            // push flow or relabel distance of current node
+            pushRelabel(G, currentNode, delta);
+            // select another node from updated graph
+            currentNode = selectNode(G, source, sink, delta);
+        }
+        delta /= 2;  // update delta if no other selectable node
+    }
+
+}
+
 void preprocess(Graph &G, Vertex source, Vertex sink) {
 
     // Store initial distances in this vector
@@ -152,6 +246,19 @@ void preprocess(Graph &G, Vertex source, Vertex sink) {
     G[source].distance = (int) num_vertices(G);
 }
 
+float calcInitialDelta(Graph &G) {
+
+    std::vector<int> capacities(num_edges(G));
+
+    Graph::edge_iterator edgeIt, edgeEnd;
+    tie(edgeIt, edgeEnd) = edges(G);
+    for (; edgeIt!= edgeEnd; ++edgeIt) {
+        capacities.push_back(G[*edgeIt].residual);
+    }
+    int maxCap = *max_element(capacities.begin(), capacities.end());
+    return (float) pow(2, ceil(log2(maxCap)));
+}
+
 Vertex selectNode(Graph &G, Vertex source, Vertex sink, float delta) {
 
     double min = std::numeric_limits<double>::infinity();
@@ -160,6 +267,8 @@ Vertex selectNode(Graph &G, Vertex source, Vertex sink, float delta) {
     Graph::vertex_iterator vertexIt, vertexEnd;
     tie(vertexIt, vertexEnd) = vertices(G);
     for (; vertexIt != vertexEnd; ++vertexIt) {
+
+        // if node has large excess (cannot be source or sink)
         if(G[*vertexIt].excess >= delta/2 && *vertexIt != source && *vertexIt != sink) {
             if(G[*vertexIt].excess < min) {
                 min = G[*vertexIt].excess;
@@ -167,6 +276,7 @@ Vertex selectNode(Graph &G, Vertex source, Vertex sink, float delta) {
             }
         }
     }
+    //return node with min excess amongst nodes with large excess
     return minNode;
 }
 
@@ -180,7 +290,8 @@ void pushRelabel(Graph &G, Vertex current, float delta) {
     for(; outedgeIt != outedgeEnd; ++outedgeIt) {
         Vertex target = boost::target(*outedgeIt, G);
 
-        if(G[current].distance == G[target].distance + 1) {  // if network contains an admissable arc
+        // if network contains an admissable arc
+        if(G[current].distance == G[target].distance + 1) {
             int pushFlow = 0;
             if(delta - G[target].excess > 0) {
                 pushFlow = std::min({G[current].excess, G[*outedgeIt].residual, (int) delta - G[target].excess});  // last arg ensures that excess doesnt exceed delta
@@ -216,8 +327,6 @@ void pushRelabel(Graph &G, Vertex current, float delta) {
 
     return;
 }
-
-
 
 
 
